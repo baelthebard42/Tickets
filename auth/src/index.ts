90c227079735431a841c72fcdf4f4f07580ccea9
baelthebard42 <078bct012.anjal@pcampus.edu.np>
2025-06-02 10:54:20 +0545

    k8 setup for ticketing service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,11 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("No env vars loaded");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("No env vars for mongo loaded");
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth"); // clusterIP of the db service we created before
+    await mongoose.connect(process.env.MONGO_URI); // clusterIP of the db service we created before
   } catch (err) {
     console.error(err);
   }
