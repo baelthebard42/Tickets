36fd8acfe3540e15f8d6d5d094d8d66fe6c6e669
baelthebard42 <078bct012.anjal@pcampus.edu.np>
2025-06-17 21:23:25 +0545

    event publishing setup, works good

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -9,6 +10,14 @@ const start = async () => {
     throw new Error("No env vars for mongo loaded");
   }
   try {
+    await natsWrapper.connect("ticketing", "asdf", "http://nats-srv:4222"); // clusterIP of the nats service we created before
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
     await mongoose.connect(process.env.MONGO_URI); // clusterIP of the db service we created before
   } catch (err) {
     console.error(err);
