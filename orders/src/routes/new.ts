e7fba65ec12ef49a59e795ad5d7d6af2be915b50
baelthebard42 <078bct012.anjal@pcampus.edu.np>
2025-06-21 15:55:39 +0545

    building Orders service: ticket and order models

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,45 @@
+import {
+  BadRequestError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+  validateRequest,
+} from "@anjal_tickets/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import mongoose from "mongoose";
+import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    //check if the order exists already
+
+    const isReserved = await ticket.isReserved();
+    if (!isReserved) {
+      throw new BadRequestError("Ticket already reserved");
+    }
+  }
+);
 
 export { router as newOrderRouter };
